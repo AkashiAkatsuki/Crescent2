483a8e3711995b2ad707b10e0226d8f59dcc0d2c
AkashiAkatsuki <4kashi4katsuki@gmail.com>
2020-08-09 18:20:23 +0900

    モデルWordとMarkovを追加

--- crescent/apps/api/models.py
+++ crescent/apps/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    name = models.TextField()
+    category = models.TextField()
+    value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'words'
+    def __str__(self):
+        return str(self.name)
+
+class Markov(models.Model):
+    prefix1 = models.IntegerField()
+    prefix2 = models.IntegerField()
+    suffix = models.IntegerField()
+    class Meta:
+        db_table = 'markovs'
