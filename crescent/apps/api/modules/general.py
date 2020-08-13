13fd1217b311925e3e9297cbe7de4ffc3d721cc1
AkashiAkatsuki <4kashi4katsuki@gmail.com>
2020-08-13 18:28:41 +0900

    トークナイザ時の品詞を番号にした

--- crescent/apps/api/modules/general.py
+++ crescent/apps/api/modules/general.py
@@ -1,12 +1,28 @@
 import MeCab
 
+CATEGORIES = [
+    "名詞",
+    "動詞",
+    "形容詞",
+    "副詞",
+    "助詞",
+    "接頭詞",
+    "助動詞",
+    "連体詞",
+    "感動詞",
+    "*",
+]
+
+def category2number(category):
+    return CATEGORIES.index(category)
+
 def tokenize(text, with_category=False):
     mecab = MeCab.Tagger()
     mecab_results = mecab.parse(text).split('\n')[:-2]
     def parse_mecab_result(mecab_result):
         name, descriptions = mecab_result.split('\t')
         category = descriptions.split(',')[0]
-        return name, category
+        return name, category2number(category)
     if with_category:
         return [parse_mecab_result(m) for m in mecab_results]
     return [parse_mecab_result(m)[0] for m in mecab_results]
--- crescent/apps/api/tests.py
+++ crescent/apps/api/tests.py
@@ -9,4 +9,4 @@ class ModuleTests(TestCase):
     def test_tokenize(self):
         tokens = tokenize("テストです", with_category=True)
         self.assertIs(tokens[0][0] == "テスト", True)
-        self.assertIs(tokens[0][1] == "名詞", True)
+        self.assertIs(tokens[0][1] == 0, True)
