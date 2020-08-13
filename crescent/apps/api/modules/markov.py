6fa3d5b838b1dc65fd2d24e62404a016036fc15e
AkashiAkatsuki <4kashi4katsuki@gmail.com>
2020-08-13 20:13:16 +0900

    対話モデルのベースクラスとマルコフモデルクラスを作成

---
+++ crescent/apps/api/modules/markov.py
@@ -0,0 +1,19 @@
+from .general import tokenize
+from apps.api.models import Word, Markov
+
+class ConversationModelBase:
+    def learn(self, input_text):
+        pass
+
+    def generate(self, input_text=None):
+        pass
+
+class MarkovModel(ConversationModelBase):
+    def learn(self, input_text):
+        pass
+
+    def generate(self, input_text=None, keyword=None):
+        pass
+
+    def _token2word(self, name, category):
+        pass
