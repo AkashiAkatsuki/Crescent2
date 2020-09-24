ba413796c0df421b25998b7740878ec9787bf199
AkashiAkatsuki <4kashi4katsuki@gmail.com>
2020-09-25 03:42:00 +0900

    品詞が文字列フィールドになっていたのを修正

--- crescent/apps/api/models.py
+++ crescent/apps/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Word(models.Model):
     name = models.TextField()
-    category = models.TextField()
+    category = models.IntegerField()
     value = models.FloatField(default=0.5)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
